15450d747cad9eac6d9a3688031efd42645dec66
kantee.ch <kantee.ch@cjdfintech.com>
2019-08-29 13:01:28 +0700

    Fragment to callback remote config

--- app/src/main/java/com/cjdfintech/merchantlauncher/Information/OtherFragment.kt
+++ app/src/main/java/com/cjdfintech/merchantlauncher/Information/OtherFragment.kt
@@ -1,18 +1,17 @@
 package com.cjdfintech.merchantlauncher.Information
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cjdfintech.merchantlauncher.BuildConfig
 import com.cjdfintech.merchantlauncher.R
+import com.cjdfintech.merchantlauncher.RemoteConfig
+import com.cjdfintech.merchantlauncher.RemoteConfigInterface
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.android.synthetic.main.fragment_other.view.*
 
-class OtherFragment : Fragment() {
+class OtherFragment : Fragment(), RemoteConfigInterface {
 
     private lateinit var rootView: View
 
@@ -25,23 +24,7 @@ class OtherFragment : Fragment() {
     }
 
     private fun getMessage(){
-        remoteConfig = FirebaseRemoteConfig.getInstance()
-        val configSettings = FirebaseRemoteConfigSettings.Builder()
-            .setDeveloperModeEnabled(BuildConfig.DEBUG)
-            .setMinimumFetchIntervalInSeconds(4200)
-            .build()
-        remoteConfig.setConfigSettings(configSettings)
-
-        remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
-            if(task.isSuccessful){
-                setupUpdateView()
-                Log.e("FirebaseRemote", "Successful!")
-            }
-            else{
-                setupUpdateView()
-                Log.e("FirebaseRemote", "Error!")
-            }
-        }
+        RemoteConfig(this).fetchRemoteConfig()
     }
 
     private fun setupUpdateView(){
@@ -53,6 +36,15 @@ class OtherFragment : Fragment() {
             rootView.nextupdate_tv.text = getString(R.string.no_update_schedule)
             rootView.instruction_layout.visibility = View.GONE
         }
+    }
+
+    override fun onSuccessFetchRemoteConfig(remoteConfig: FirebaseRemoteConfig) {
+        this.remoteConfig = remoteConfig
+        setupUpdateView()
+    }
+
+    override fun onFailedFetchRemoteConfig() {
+        setupUpdateView()
     }
 
     override fun onResume() {
